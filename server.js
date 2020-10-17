5f5a1d12c9c3e95bbd561241a20a67f6b37a2077
santosh thapa <s7624589@gmail.com>
2020-10-17 18:45:38 +0545

    add-cart-page

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app =express()
 const ejs = require('ejs')
 const path =require('path')
-const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000;
 
 
@@ -10,16 +9,31 @@ const PORT = process.env.PORT || 3000;
 //assest
 app.use( express.static('public'))
 
-app.get('/',function(req,res){
-    res.render('home')
-})
+
 
 //set Template engines setup
-app.use(expressLayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
 
 
+app.get('/',function(req,res){
+    res.render('home')
+})
+
+app.get('/cart',function(req,res){
+    res.render('customers/cart')
+
+})
+
+app.get('/login',function(req,res){
+    res.render('auth/login')
+
+})
+
+app.get('/register',function(req,res){
+    res.render('auth/register')
+
+})
 
 
 
